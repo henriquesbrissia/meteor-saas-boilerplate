f1568cf70ecc0ba036b526f4eb57ccb0f0df6f2e
henriquesbrissia <henriqueerics@gmail.com>
2024-11-25 09:43:16 -0300

    feat: add base routes

--- client/main.tsx
+++ client/main.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Meteor } from 'meteor/meteor';
-import { App } from '/imports/ui/App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Meteor } from "meteor/meteor";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-const queryClient = new QueryClient()
+import { SignIn } from "/imports/ui/pages/SignIn";
+import { SignUp } from "/imports/ui/pages/SignUp";
+import { ROUTES } from "/imports/ui/utils/routes";
+
+const queryClient = new QueryClient();
 
 Meteor.startup(() => {
-  const container = document.getElementById('react-target');
+  const container = document.getElementById("react-target");
   const root = createRoot(container!);
   root.render(
-  <QueryClientProvider client={queryClient}>
-  <App />
-  </QueryClientProvider>);
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Navigate to={ROUTES.SIGN_IN} replace />} />
+          <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 });
-
