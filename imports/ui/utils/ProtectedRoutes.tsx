8f2501a0c15236963760adb06025e5e201134ac9
henriquesbrissia <henriqueerics@gmail.com>
2024-11-26 22:22:47 -0300

    feature!: persist user logged in

--- imports/ui/utils/ProtectedRoutes.tsx
+++ imports/ui/utils/ProtectedRoutes.tsx
@@ -1,5 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import { useTracker } from "meteor/react-meteor-data";
+import { Suspense } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 import { ROUTES } from "./routes";
@@ -7,5 +8,11 @@ import { ROUTES } from "./routes";
 export const ProtectedRoutes = () => {
   const user = useTracker(() => Meteor.user());
 
-  return user ? <Outlet /> : <Navigate to={ROUTES.SIGN_IN} />;
+  return user !== null || user !== undefined ? (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Outlet />
+    </Suspense>
+  ) : (
+    <Navigate to={ROUTES.SIGN_IN} />
+  );
 };
