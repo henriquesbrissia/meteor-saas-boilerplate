08038e884034b9f8d12b1b40ea1a2f8540ef67b8
henriquesbrissia <henriqueerics@gmail.com>
2024-12-01 12:38:44 -0300

    fix: removes password console.log security gap

--- imports/api/auth/methods.ts
+++ imports/api/auth/methods.ts
@@ -8,10 +8,8 @@ export const authModule = createModule("auth")
   .addMethod("signUp", authSignUpSchema, async ({ email, password }) => {
     try {
       await Accounts.createUserAsync({ email, password });
-      console.log("success", password, email);
     } catch (e) {
       const error = e as Meteor.Error;
-      console.log("Error signing up", error);
       throw new Meteor.Error(error.message);
     }
   })
